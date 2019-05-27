ca1aa83a279298939da1161ba82cdd20559ef634
Vian Chen <imvianchen@outlook.com>
2019-05-27 17:01:36 +0800

    Document work

--- src/main/java/Parser/Parser.java
+++ src/main/java/Parser/Parser.java
@@ -21,6 +21,8 @@ import java.util.Map;
 import java.util.logging.Level;
 
 /**
+ * DO NOT construct multiple parsers at the same time. See {@code implNote} for why.
+ *
  * @implNote The {@code Parser} class is implemented using Selenium's interface. Basically,
  * when a parser instance is created, the parser will try to initiate a new Chromium browser
  * instance and manipulate it. However, the Chromium instance must be explicitly closed.
@@ -57,12 +59,11 @@ public class Parser implements Closeable {
         // construct Chromium instance
         this(true);
 
-        // Mimic a user logging in
+        // ======================= Mimic a user logging in =======================
         driver.get("https://e.buaa.edu.cn/users/sign_in");
         driver.findElement(By.id("user_login")).sendKeys(username);
         driver.findElement(By.id("user_password")).sendKeys(password);
         driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[3]/input")).click();
-
 
         // Do not act until login finished. the {@code timeOutInSeconds} in this call means that after
         // this many seconds later, if the expectation is still unfulfilled, then the function should
@@ -72,9 +73,14 @@ public class Parser implements Closeable {
         new WebDriverWait(driver, 10, 200)
                 .until(ExpectedConditions.urlToBe("https://e.buaa.edu.cn/"));
 
-        // Get token to the platform
+
+        // ====================== Get token to the platform ======================
         driver.get("https://10-200-21-61-7001.e.buaa.edu.cn/ieas2.1/");
+
+        // Click on the login button
         driver.findElement(By.xpath("//*[@id=\"notice\"]/div[2]/div[1]/p[2]/input")).click();
+
+        // Wait for it to finish loading
         new WebDriverWait(driver, 10, 200)
                 .until(ExpectedConditions.urlToBe("https://10-200-21-61-7001.e.buaa.edu.cn/ieas2.1/welcome"));
     }
@@ -105,11 +111,18 @@ public class Parser implements Closeable {
      */
     private void parse(@NotNull Map<String, Boolean[]> map, @NotNull Document document) {
         Elements table = document.select("body > div > div > div.list > table > tbody").first().children();
-        List<Element> elList = Lists.newArrayList(table.iterator());
 
-        for (int i = 2; i < elList.size(); ++i) {
-            List<Element> currentRow = Lists.newArrayList(elList.get(i).children().iterator());
-            Boolean[] available = new Boolean[42];
+        // Convert `Elements` to a `List<Element>` so that it will be easier to ignore the
+        // first two rows.
+        List<Element> rows = Lists.newArrayList(table.iterator());
+
+        // 6 ranges: [1, 2], [3, 4, 5], [6, 7], [8, 9, 10], [11, 12], [13, 14] and 7 days
+        final int ROW_CAPACITY = 6 * 7;
+
+        // Starts from 2 because the first two rows are useless.
+        for (int i = 2; i < rows.size(); ++i) {
+            List<Element> currentRow = Lists.newArrayList(rows.get(i).children().iterator());
+            Boolean[] available = new Boolean[ROW_CAPACITY];
             for (int j = 1; j < currentRow.size(); ++j) {
                 available[j - 1] = currentRow.get(j).child(0).hasClass("kjs_icon");
             }
